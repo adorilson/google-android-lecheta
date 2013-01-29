6a514954cb965f551565d2203e25f0728fcb1144
Adorilson Bezerra <adorilson@gmail.com>
2013-01-29 19:54:20 -0300

    - Criacao da inferface visual com a API Java (tópico 3.6)

--- LivroAndroidCap3/src/br/livro/android/cap3/Exemplo2.java
+++ LivroAndroidCap3/src/br/livro/android/cap3/Exemplo2.java
@@ -3,13 +3,19 @@ package br.livro.android.cap3;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class Exemplo2 extends Activity {
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
-		setContentView(R.layout.layout_exemplo2);
+		
+		// Cria o TextView para exibir na tela
+		TextView view = new TextView(this);
+		view.setText("Exemplo de layout feito com a API Java");
+		
+		setContentView(view);
 	}
 
 	@Override
