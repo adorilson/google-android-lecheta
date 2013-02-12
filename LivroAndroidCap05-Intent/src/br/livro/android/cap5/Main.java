3f1b3b6221a9484c8634968046e8f8d075a90659
Adorilson Bezerra <adorilson@gmail.com>
2013-02-12 20:32:37 -0300

    - Adicao de activity Main para conter todos os botoes da secao Tambem foi implementado a ligacao para um numero de telefone (topico 5.5)

---
+++ LivroAndroidCap05-Intent/src/br/livro/android/cap5/Main.java
@@ -0,0 +1,60 @@
+package br.livro.android.cap5;
+
+import java.util.List;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class Main extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+		
+		// Busca o TextView pelo id
+		final EditText textNome = (EditText) findViewById(R.id.campoEndereco);
+		Button button = (Button) findViewById(R.id.botaoOk);
+		// Informa o listener
+		button.setOnClickListener( new Button.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				EditText campoEndereco = (EditText) findViewById(R.id.campoEndereco);
+				String endereco = campoEndereco.getText().toString();
+				//Representa o endereço que desejamos abrir
+				Uri uri = Uri.parse(endereco);
+				// Cria a Intent com o endereco
+				Intent intent = new Intent(Intent.ACTION_VIEW, uri);
+				// Envia a mensagem ao sistema operacional
+				startActivity(intent);
+			}
+		});
+		
+		// Busca o Button pelo id
+		Button button_chamada = (Button) findViewById(R.id.botaoChamada);
+		// Informa o listener
+		button_chamada.setOnClickListener( new Button.OnClickListener(){
+			@Override
+			public void onClick(View v){
+				final EditText campoTelefone = (EditText) findViewById(R.id.campoTelefone);
+				String telefone = campoTelefone.getText().toString();
+				//Representa o telefone que desejamos ligar
+				Uri uri = Uri.parse("tel:" + telefone);
+				// Cria a Intent com o telefoen
+				Intent intent = new Intent(Intent.ACTION_CALL, uri);
+				// Envia a mensagem ao sistema operacional
+				startActivity(intent);
+			}
+		});	
+		
+	}
+
+}
